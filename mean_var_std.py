df03848e4fb5decd97fbbb43cbe4ba2bf1daf783
Tyler Whitaker <tywhit@outlook.com>
2020-09-26 13:50:05 -0600

    Wrote code to ensure 9 numbers in list

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,7 @@
 import numpy as np
 
 def calculate(list):
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
 
-
-
-
-    return calculations
+    return #calculations
