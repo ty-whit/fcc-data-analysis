9f0e056f2bf84e5b733e60b511e05b5d49d349d9
Tyler Whitaker <tywhit@outlook.com>
2020-09-29 09:51:18 -0600

    Added code to calculate function.

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,7 +1,23 @@
 import numpy as np
 
-def calculate(list):
-    if len(list) != 9:
+def calculate(listNums):
+    if len(listNums) != 9:
         raise ValueError("List must contain nine numbers.")
 
-    return #calculations
+    matrix = np.reshape(listNums, (3, 3))
+
+    calculations = {}
+    calculations['mean'] = compute_function(matrix, np.mean)
+    calculations['variance'] = compute_function(matrix, np.var)
+    calculations['standard deviation'] = compute_function(matrix, np.std)
+    calculations['max'] = compute_function(matrix, np.max)
+    calculations['min'] = compute_function(matrix, np.min)
+    calculations['sum'] = compute_function(matrix, np.sum)
+
+    return calculations
+
+def compute_function(matrix,function):
+    calculations = [function(matrix, axis=0).tolist(), 
+                    function(matrix, axis=1).tolist(), 
+                    function(matrix)]
+    return calculations
