c02d7920dd8513ccb192970e8c123c7c27ae17c6
twhitaker <twhitaker@entrata.com>
2021-02-08 13:59:15 -0700

    Built function to plot line graph

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -15,14 +15,16 @@ df = df[(df['value'] > df['value'].quantile(0.025)) & (df['value'] < df['value']
 
 def draw_line_plot():
     # Draw line plot
-
-
-
-
+    fig2 = plt.subplots(1)
+    fig2 = sns.lineplot(data=df)
+    fig2.set_title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+    fig2.set_xlabel('Date')
+    fig2.set_ylabel('Page Views')
 
     # Save image and return fig (don't change this part)
-    fig.savefig('line_plot.png')
-    return fig
+    fig2.figure.savefig('line_plot.png')
+    return fig2
+
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
